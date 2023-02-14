dc650032ab1e2439eb701d0dd12c7919e32cf5b5
fkrein1 <felipekrein@gmail.com>
2023-02-14 11:30:52 -0300

    feat: add jost font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
+import {
+  Jost_400Regular,
+  Jost_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/jost';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Welcome } from './src/screens/Welcome';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <Welcome />
       <StatusBar />
     </SafeAreaView>
---
+++ src/styles/fonts.ts
@@ -0,0 +1,6 @@
+export const fonts = {
+  heading: 'Jost_600SemiBold',
+  text: 'Jost_400Regular',
+  complement: 'Jost_400Regular',
+
+};
