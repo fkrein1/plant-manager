d8d1d21e748f623e9ca3a17d51a762842ac5a4a7
fkrein1 <felipekrein@gmail.com>
2023-02-14 16:06:57 -0300

    refactor: welcome page

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,7 +1,15 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import wateringImg from '../assets/watering.png';
-import { Button } from '../components/Button';
 import { colors } from '../styles/colors';
+import { fonts } from '../styles/fonts';
 
 export function Welcome() {
   return (
@@ -10,14 +18,19 @@ export function Welcome() {
         Easily {'\n'} manage your {'\n'}plants
       </Text>
 
-      <Image style={styles.image} source={wateringImg} />
+      <Image style={styles.image} source={wateringImg} resizeMode="contain" />
 
       <Text style={styles.subtitle}>
         Don't forget to water your plants anymore. We'll take care of reminding
         you whenever you need it.
       </Text>
 
-      <Button title=">" />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>
+          {' '}
+          <Feather name="chevron-right" size={32} />{' '}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -26,23 +39,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
     marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     marginHorizontal: 44,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   image: {
-    width: 292,
-    height: 284,
+    width: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 });
