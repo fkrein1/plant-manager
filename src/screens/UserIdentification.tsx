aacfe3c39d12e40db8d9c0a3c546026d817889b2
fkrein1 <felipekrein@gmail.com>
2023-02-14 18:01:16 -0300

    fix: exit keyboard on userIdentification screen

--- src/screens/UserIdentification.tsx
+++ src/screens/UserIdentification.tsx
@@ -1,11 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
 import {
+  Keyboard,
   KeyboardAvoidingView,
   Platform,
   StyleSheet,
   Text,
   TextInput,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import { Button } from '../components/Button';
@@ -33,25 +35,29 @@ export function UserIdentification() {
 
   return (
     <KeyboardAvoidingView
-      style={styles.container}
+      style={{ flex: 1 }}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <Text style={styles.emoji}>{isFilled ? '😄' : '😀'}</Text>
-      <Text style={styles.heading}>What can we {'\n'} call you?</Text>
-      <TextInput
-        style={[
-          styles.input,
-          (isFocused || isFilled) && { borderBottomColor: colors.green },
-        ]}
-        placeholder="Enter yout name"
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        value={name}
-        onChangeText={setName}
-      />
-      <View style={styles.footer}>
-        <Button title="Confirm" onPress={handleSubmit} />
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text style={styles.emoji}>{isFilled ? '😄' : '😀'}</Text>
+          <Text style={styles.heading}>What can we {'\n'} call you?</Text>
+          <TextInput
+            style={[
+              styles.input,
+              (isFocused || isFilled) && { borderBottomColor: colors.green },
+            ]}
+            placeholder="Enter yout name"
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            value={name}
+            onChangeText={setName}
+          />
+          <View style={styles.footer}>
+            <Button title="Confirm" onPress={handleSubmit} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 }
@@ -64,6 +70,7 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingHorizontal: 56,
   },
+
   emoji: {
     fontSize: 44,
     marginBottom: 24,
